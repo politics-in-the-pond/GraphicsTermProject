607a205baa470a43c7a4320b726763f832fc8bdc
khjj07 <khjj07@gmail.com>
2021-11-04 00:17:03 +0900

    assetsLoader 및 eventlistener 테스트

--- UITest.js
+++ UITest.js
@@ -1,29 +1,58 @@
-
-
 function UIInit(renderer)
 {
     document.body.appendChild(renderer.domElement);
     return new ThreeUI(renderer.domElement, 720);
 }
+// AssetLoader
+   // AssetLoader.add.webFont('webFont', 'fonts/web-font.css');
+   AssetLoader.add.image('sprites/heart.png');
+   // AssetLoader.add.image('sprites/asset-active.png');
+   // AssetLoader.add.spriteSheet('sprites/sheet.png', 'sprites/sheet.json');
+   // AssetLoader.add.bitmapText('fonts/bitmap-font.png', 'fonts/bitmap-font.json');
 
+    // Set a progress listener, can be used to create progress bars
+    AssetLoader.progressListener = function(progress) {
+        console.info('Progress: ' + (progress * 100) + '%');
+    }; //progress바 표기
+
+    AssetLoader.load(function() { //에셋 로더
+        // Initialize the game
+        init();
+    });
+    
+    
 //testcode
-window.onload = function init() 
+function init() 
 {
   const canvas = document.getElementById( "gl-canvas" );
   const renderer = new THREE.WebGLRenderer({canvas});
+  renderer.setSize(window.innerWidth, window.innerHeight); //ui anchor를 위해 필수
+
   const ui = UIInit(renderer);
   const fov = 75;
   const aspect = 2;  // the canvas default
   const near = 0.1;
   const far = 5;
+
   // 카메라 세팅
-  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far); //카메라 객체 생성
-  camera.position.z = 2;
+    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far); //카메라 객체 생성
+    camera.position.z = 2;
+    
 
-  // UI elements
-  const rectangle = ui.createRectangle('#FF6D92', 0, 0, 250, 250)
-
-
+    // UI elements
+    const rectangle = ui.createRectangle('#FF6D92',0, 0, 300, 100)//x,y,w,h
+    rectangle.anchor.x = ThreeUI.anchors.center;
+    rectangle.anchor.y = ThreeUI.anchors.center;
+    const text = ui.createText('BUTTON', 40, 'Arial', 'white');
+    text.textAlign = 'center';
+    text.textBaseline = 'middle';
+    text.anchor.x = ThreeUI.anchors.center;
+    text.anchor.y = ThreeUI.anchors.center;
+    text.parent = rectangle;
+    rectangle.onClick(() => {
+        console.info('clicked');
+    });
+    //scene
   const scene = new THREE.Scene();
   {
     const color = 0xFFFFFF;
@@ -32,6 +61,7 @@ window.onload = function init()
     light.position.set(-1, 2, 4);
     scene.add(light);
   }
+  
 
   const boxWidth = 1;
   const boxHeight = 1;
@@ -46,9 +76,11 @@ window.onload = function init()
     return cube;
   }
 
+  
   const cubes = [
     makeInstance(geometry, 0x44aa88,  0),
   ];
+
 
   function render(time) {
     time *= 0.001;  // convert time to seconds
@@ -60,7 +92,6 @@ window.onload = function init()
     });
     ui.render(renderer);
     renderer.render(scene, camera);
-
     requestAnimationFrame(render);
   }
   requestAnimationFrame(render);	
